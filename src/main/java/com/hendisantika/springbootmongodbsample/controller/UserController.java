fe4eee1e84dc5f0de563829add428cf2ce0a80f5
Hendi Santika <hendisantika@gmail.com>
2019-01-25 06:21:25 +0700

    #7 - Add Get User in User Controller

--- src/main/java/com/hendisantika/springbootmongodbsample/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootmongodbsample/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.hendisantika.springbootmongodbsample.controller;
 
+import com.hendisantika.springbootmongodbsample.model.User;
 import com.hendisantika.springbootmongodbsample.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +30,11 @@ public class UserController {
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        LOG.info("Getting all users.");
+        return userRepository.findAll();
     }
 }
